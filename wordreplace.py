aefb6092add5b5423262170d19da48dbd4a443e0
Jemimah Nagasha <jemynagasha@gmail.com>
2023-10-20 10:18:07 +0300

    word replacement program

---
+++ wordreplace.py
@@ -0,0 +1,11 @@
+# print('Hello World')
+
+# name = input('Input Your Name: ')
+# age = input('Input Your Age: ')
+# print('Your name is ' + name + 'and you are ' + age + 'years old')
+
+sentence = input('Enter your sentence: ')
+print('Your sentence is: '+sentence)
+word1 = input('Enter word to replace: ')
+word2 = input('Enter new word: ')
+print(sentence.replace(word1, word2))
